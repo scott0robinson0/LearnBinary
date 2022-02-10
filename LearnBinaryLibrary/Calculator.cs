6731ba73860b0c425ed16be5b6dafce7886c777d
Scott Robinson <scott0robinson0@gmail.com>
2022-02-10 22:43:52 +0000

    Testing Calculator

---
+++ LearnBinaryLibrary/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnBinaryLibrary
+{
+    public class Calculator
+    {
+        private byte value1_;
+        private byte value2_;
+        public Calculator(byte val1, byte val2)
+        {
+            value1_ = val1;
+            value2_ = val2;
+        }
+
+        public byte Add()
+        {
+            return (byte)(value1_ + value2_);
+        }
+
+        public byte Sub()
+        {
+            return (byte)(value1_ - value2_);
+        }
+
+        public byte Mul()
+        {
+            return (byte)(value1_ * value2_);
+        }
+
+        public byte Div()
+        {
+            return (byte)(value1_ / value2_);
+        }
+    }
+}
---
+++ LearnBinaryTesting/CalculatorTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LearnBinaryLibrary;
+
+namespace LearnBinaryTesting
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        private static readonly Calculator calculator = new Calculator(50, 2);
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            Assert.AreEqual(calculator.Add(), 52);
+        }
+
+        [TestMethod]
+        public void TestSub()
+        {
+            Assert.AreEqual(calculator.Sub(), 48);
+        }
+
+        [TestMethod]
+        public void TestMul()
+        {
+            Assert.AreEqual(calculator.Mul(), 100);
+        }
+
+        [TestMethod]
+        public void TestDiv()
+        {
+            Assert.AreEqual(calculator.Div(), 25);
+        }
+    }
+}
