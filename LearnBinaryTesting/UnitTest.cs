a88cade2b27258201053ed7598c42a1447c8140e
Scott Robinson <scott0robinson0@gmail.com>
2022-02-10 21:21:59 +0000

    Initial

---
+++ LearnBinaryTesting/UnitTest.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LearnBinaryTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
